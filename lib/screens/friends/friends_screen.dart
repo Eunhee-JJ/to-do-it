a9f70c65c115cd346a4a20e7db926807c4fce42e
Eunhee-JJ <eunhee0324@g.skku.edu>
2023-05-24 21:32:42 +0900

    feat load contacts

--- lib/screens/friends/friends_screen.dart
+++ lib/screens/friends/friends_screen.dart
@@ -16,45 +16,60 @@ class _FriendsScreenState extends State<FriendsScreen> {
   ];
   List<Contact> contacts = [];
 
-  Future<void> roadContacts() async {
+  Future<String> roadContacts() async {
     // Request contact permission
     if (await FlutterContacts.requestPermission()) {
       // Get all contacts (lightly fetched)
       contacts = await FlutterContacts.getContacts();
     }
+    return 'Call Data';
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          Container(
-              padding:
-                  EdgeInsets.only(left: 35, right: 35, top: 30, bottom: 30),
-              child: Text(
-                "친구 목록",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-              )),
-          Container(
-            padding: EdgeInsets.only(left: 35, right: 35, top: 30, bottom: 30),
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: contacts.length,
-                itemBuilder: (context, index) {
-                  print(contacts.length);
-                  return ListTile(
-                    title: Text(contacts[index].displayName),
-                    trailing: TextButton(
-                      child: Text("추가"),
-                      onPressed: () {},
+    return FutureBuilder(
+        future: roadContacts(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold(
+                body: SafeArea(
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                        padding: EdgeInsets.only(
+                            left: 35, right: 35, top: 30, bottom: 10),
+                        child: Text(
+                          "친구 목록",
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.w500),
+                        )),
+                  ),
+                  Expanded(
+                    flex: 17,
+                    child: Container(
+                      padding: EdgeInsets.only(left: 20, right: 10, bottom: 10),
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: contacts.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text(contacts[index].displayName),
+                              trailing: TextButton(
+                                child: Text("추가"),
+                                onPressed: () {},
+                              ),
+                            );
+                          }),
                     ),
-                  );
-                }),
-          ),
-        ],
-      ),
-    ));
+                  )
+                ],
+              ),
+            ));
+          } else {
+            return Text("연락처 목록을 불러오는 데에 실패했습니다.");
+          }
+        });
   }
 }
